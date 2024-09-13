c992900ae9735e6fd5994fa0649309da8f065a57
Fabian <frakxman@hotmail.com>
2024-09-13 17:50:47 -0500

    Aadd new modules and dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,30 @@
 import { Module } from '@nestjs/common';
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
+
+
 
 @Module({
-  imports: [ProductsModule, UsersModule],
+  imports: [HttpModule, ProductsModule, UsersModule, DatabaseModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'TASKS',
+      useFactory: async (Http: HttpService) => {
+        const request = await Http.get('https://jsonplaceholder.typicode.com/todos');
+        const tasks = await lastValueFrom(request);
+        return tasks.data;
+      },
+      inject: [HttpService],
+    },
+  ],
 })
 export class AppModule {}
